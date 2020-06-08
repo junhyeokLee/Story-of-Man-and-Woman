6d6533e7d465e5484c66cb1eeca4b228b5bd6fa7
junhyeokLee <junhyeoklee616@gmail.com>
2020-06-08 17:03:06 +0900

    Create project

---
+++ DevSheep/app/src/main/java/com/dev_sheep/story_of_man_and_woman/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dev_sheep.story_of_man_and_woman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
